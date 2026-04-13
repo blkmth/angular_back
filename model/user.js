b4ea34afe559f4830daee9449f2d150dd0d1c5de
Math Charles N'guessan <mathcharlesnguessan@gmail.com>
2026-04-13 13:19:48 +0000

    creation du user.js pour representer les users dans la bd

---
+++ model/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+// methode pour comparé un mot  de passe saisi avec le hash stocké
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
